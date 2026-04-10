52de604ce8fbd8387109b72e860e53b71724af58
Akaikumogo <98486553+Akaikumogo@users.noreply.github.com>
2026-04-10 14:04:39 +0500

    chore: uncomment baseURL definition for local development in api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+// const baseURL = import.meta.env.VITE_API_URL ?? 'http://localhost:3000';
 const baseURL =
   import.meta.env.VITE_API_URL ?? 'https://crm-pro-backend.akaikumogo.uz';
 
